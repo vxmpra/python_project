0e5e3704d5cb3ed748d13067bd43e21b5de22d36
vxmpra <kolmykova_sasha19@mail.ru>
2026-01-09 16:52:46 +0500

    Add login.html template

--- garden_service/urls.py
+++ garden_service/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from plants import views
+from plants.views import home
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='base'),
     path('plants/', include('plants.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', next_page='plant_list'), name='login'),
 ]
