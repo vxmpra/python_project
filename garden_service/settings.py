2323974470809b6c262f3b88f236aef92e461c78
vxmpra <kolmykova_sasha19@mail.ru>
2026-01-16 22:57:38 +0500

    Improve watering recommendations with more detailed explanations

--- garden_service/settings.py
+++ garden_service/settings.py
@@ -85,9 +85,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-    'localhost',
-]
